c5d351bdfde6ae8ba2d0e2f5862ef814fe291004
crimsonskies <cskies1441@gmail.com>
2022-03-25 13:22:22 +0530

    Fix Recruiter

--- src/Components/Recruiters.jsx
+++ src/Components/Recruiters.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Recruiters.css'
 import { Container } from 'react-bootstrap';
 import AkerSol from '../img/tnpsite-logo/Aker-Solutions.png';
- import alfalaval from '../img/tnpsite-logo/alfa-laval.webp';
+import alfalaval from '../img/tnpsite-logo/alfa-laval.webp';
 import analogic from '../img/tnpsite-logo/analogic.jpg';
 import atossyntelvectorlogo from '../img/tnpsite-logo/atos-syntel-vector-logo.png';
 import cognizant from '../img/tnpsite-logo/cognizant.png';
@@ -29,197 +29,197 @@ import laurus from '../img/tnpsite-logo/laurus.png';
 import mahindra from '../img/tnpsite-logo/mahindra.png';
 import neml from '../img/tnpsite-logo/neml.png';
 import neologic from '../img/tnpsite-logo/neologic.png';
-import  netcracker from '../img/tnpsite-logo/netcracker.png';
+import netcracker from '../img/tnpsite-logo/netcracker.png';
 import nivida from '../img/tnpsite-logo/nivida.png';
 import persistent from '../img/tnpsite-logo/persistent.png';
 import raja from '../img/tnpsite-logo/raja.png';
 import rcf from '../img/tnpsite-logo/rcf.png';
 import reliance from '../img/tnpsite-logo/reliance.png';
-import  routerabbit from '../img/tnpsite-logo/routerabbit.png';
+import routerabbit from '../img/tnpsite-logo/routerabbit.png';
 import sandriver from '../img/tnpsite-logo/sandriver.jpg';
 import sarvaha from '../img/tnpsite-logo/sarvaha.png';
 import sch from '../img/tnpsite-logo/sch-invensys.png';
 import siemens from '../img/tnpsite-logo/siemens.png';
-import  syntel from '../img/tnpsite-logo/syntel.png';
+import syntel from '../img/tnpsite-logo/syntel.png';
 import tatamotors from '../img/tnpsite-logo/tata motors.png';
 import tataconsulting from '../img/tnpsite-logo/tataconsulting.png';
- import  tatapower from '../img/tnpsite-logo/tatapower.png';
+import tatapower from '../img/tnpsite-logo/tatapower.png';
 import TCSlogo from '../img/tnpsite-logo/TCS logo.png';
-import  texasins from '../img/tnpsite-logo/texasins.png';
- import  thermax from '../img/tnpsite-logo/thermax.png';
- import  toyoeg from '../img/tnpsite-logo/toyoeg.png';
- import  tpllogo from '../img/tnpsite-logo/tpl-logo.png';
- import  welspun from '../img/tnpsite-logo/welspun.png';
-  import  yokogawa from '../img/tnpsite-logo/yokogawa.jpg';
+import texasins from '../img/tnpsite-logo/texasins.png';
+import thermax from '../img/tnpsite-logo/thermax.png';
+import toyoeg from '../img/tnpsite-logo/toyoeg.png';
+import tpllogo from '../img/tnpsite-logo/tpl-logo.png';
+import welspun from '../img/tnpsite-logo/welspun.png';
+import yokogawa from '../img/tnpsite-logo/yokogawa.jpg';
 
 import {
-    Image, Col,Row,
+  Image, Col, Row,
 } from 'react-bootstrap';
 
 export default function Recruiters() {
-    return (<>
-        <section id="image" className="image">
-            <Container>
-                <Row>
-                    <h3>Recruiters</h3>
-                    <Col lg="2" md="3">
-                        <Image className='photo' src={AkerSol} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image  className="photo" src={alfalaval} /> 
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={analogic} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={cognizant} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={atossyntelvectorlogo} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className='photo' src={bharatforg} />
-                    </Col>
-                     <Col lg="2" md="3">
-                         <Image className='photo' src={darkhorse} /> 
-                    </Col> 
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={eleation} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={emerson} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={fox} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={geometriccompanylogo} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={grifeo} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={hcl} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={honeywell} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={hpenterprise} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={iauro} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={ibm} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={index} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={infosis} />
-                    </Col>
-                    <Col lg="2" md="3">
-                        <Image className="photo" src={jacobs} />
-                        </Col>
-                        <Col lg="2" md="3">
-                            <Image className="photo" src={jonhsoncontrols} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={kpitcummins} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={kratin} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={It} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={laurus} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={mahindra} />
-                            </Col>
-                        
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={neml} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={neologic} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={netcracker} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={yokogawa} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={persistent} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={raja} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={welspun} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={reliance} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={routerabbit} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={sandriver} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={sarvaha} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                 <Image className="photo" src={sch} /> 
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={siemens} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={syntel} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={tatamotors} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={tataconsulting} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={tatapower} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={TCSlogo} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={texasins} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={thermax} />
-                            </Col>
-                           
-                            <Col lg="2" md="3">
-                                 <Image className="photo" src={toyoeg} /> 
-                           
-                                <Image className="photo" src={tpllogo} />
-                            </Col>
-                           
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={rcf} />
-                            </Col>
-                            <Col lg="2" md="3">
-                                <Image className="photo" src={nivida} />
-                            </Col> 
+  return (<>
+    <section id="image" className="image">
+      <h3>Recruiters</h3>
+      <Container className="rec-scroll" fluid>
+        <Row>
+          <Col lg="2" md="3">
+            <Image className='photo' src={AkerSol} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={alfalaval} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={analogic} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={cognizant} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={atossyntelvectorlogo} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className='photo' src={bharatforg} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className='photo' src={darkhorse} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={eleation} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={emerson} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={fox} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={geometriccompanylogo} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={grifeo} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={hcl} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={honeywell} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={hpenterprise} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={iauro} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={ibm} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={index} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={infosis} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={jacobs} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={jonhsoncontrols} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={kpitcummins} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={kratin} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={It} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={laurus} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={mahindra} />
+          </Col>
 
-                        </Row>
-                    </Container>
-                </section>
+          <Col lg="2" md="3">
+            <Image className="photo" src={neml} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={neologic} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={netcracker} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={yokogawa} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={persistent} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={raja} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={welspun} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={reliance} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={routerabbit} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={sandriver} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={sarvaha} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={sch} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={siemens} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={syntel} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={tatamotors} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={tataconsulting} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={tatapower} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={TCSlogo} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={texasins} />
+          </Col>
+          <Col lg="2" md="3">
+            <Image className="photo" src={thermax} />
+          </Col>
+
+          <Col lg="2" md="3">
+            <Image className="photo" src={toyoeg} />
+
+            <Image className="photo" src={tpllogo} />
+          </Col>
+
+          <Col lg="2" md="3">
+            <Image className="photo" src={rcf} />
+          </Col>
+          <Col lg="1" md="3">
+            <Image className="photo" src={nivida} />
+          </Col>
+
+        </Row>
+      </Container>
+    </section>
 
 
 
-            </>)
+  </>)
 }
