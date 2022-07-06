4a0d9380c1aea72b857004479707fd6177a85b2e
crimsonskies <amodm.1102@gmail.com>
2022-07-06 19:52:02 +0530

    Update links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -32,8 +32,8 @@ export default function Footer() {
           <p> This is one of the prestigious institutes in the state of Maharashtra. This institute provides you the opportunities to empower you with the present technologies. The faculty in the institute is ever enthusiastic to familiarize the students in future technology trends. </p>
           <Stack className="about-social" direction="horizontal">
             <a href="https://m.facebook.com/profile.php"><FontAwesomeIcon size="1.5x" color="#3b5998" icon={faFacebookF} /></a>
-            <a href="https://twitter.com/tnpcell_sggsiet"><FontAwesomeIcon color="#1da1f2" icon={faTwitter} /></a>
-            <a href="https://www.instagram.com/tnpcell_sggsnanded/"><FontAwesomeIcon color="#ffc0cb" icon={faInstagram} /></a>
+            <a href="https://twitter.com/tnp_sggsiet"><FontAwesomeIcon color="#1da1f2" icon={faTwitter} /></a>
+            <a href="https://www.instagram.com/tnp_sggsiet/"><FontAwesomeIcon color="#ffc0cb" icon={faInstagram} /></a>
             <a href="https://www.youtube.com/channel/UCxK3BwLWib2jd8y80CbD4DA"><FontAwesomeIcon color="#ff0000" icon={faYoutube} /></a>
             <a href="https://www.linkedin.com/in/sggs-tnpcell/"><FontAwesomeIcon icon={faLinkedinIn} /></a>
           </Stack>
